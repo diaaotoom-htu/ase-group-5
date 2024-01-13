8a72120e829e2d8248956783d9b18471fda3c603
Hashem Altabbaa <hashemtb02@gmail.com>
2024-01-14 00:12:58 +0300

    Updating the background image of the website

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,12 +2,25 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import LoginBox from "../components/LoginBox";
 import UseToken from "../components/UseToken";
 import { Navigate } from "react-router-dom";
+import backgroundImg from '../assets/background.png';
 
 export default function Login() {
   const themeDark = createTheme({
     palette: {
       background: {
         default: "#2A2D34",
+      },
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            height: "100%",
+            backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          },
+        },
       },
     },
   });
@@ -19,7 +32,7 @@ export default function Login() {
   return (
     <ThemeProvider theme={themeDark}>
       <CssBaseline />
-      <LoginBox setToken={setToken} />;
+      <LoginBox setToken={setToken} />
     </ThemeProvider>
   );
 }
